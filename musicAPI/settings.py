155ffe61f2a3d55d6db14adc3541a501a84b19ec
Luis Henrique Rocha Bueno <luizbueno1201@gmail.com>
2021-02-01 19:33:56 -0300

    Alterações de permissões

--- musicAPI/settings.py
+++ musicAPI/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
