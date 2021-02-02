44ffaa6ea4c0d49533425aeee9dcdb2304a6fdf0
Luis Henrique Rocha Bueno <luizbueno1201@gmail.com>
2021-02-02 11:28:11 -0300

    Corrigindo problemas

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
+### Criando um SECRET_KEY
+O Django usa a SECRET_KEY para gerenciar algumas issues de segurança na nossa aplicação. Por exemplo, o Django faz a criptografia das senhas dos usuários baseado também na SECRET_KEY, cria aquelas URLs únicas de reset da senha do usuário, além de outras coisinhas na sessão, cookie e etc. 
+**Então o recomendado é nunca expor ela.**
+
+A um script criado para gerar uma nova para você sem muitas preocupações.
+```
+python generate_secret_key.py
+```
 ### Iniciando um servidor local
 ```bash
 python manage.py runserver
--- musicAPI/settings.py
+++ musicAPI/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+o$+(d_6c89%-dn$4xa7rn&ngtdd@f%+mr%g%e5fhiieqjd&b_'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -160,5 +160,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
